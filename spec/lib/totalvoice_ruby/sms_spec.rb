080e727e375f467f5a691a92bd164fdbea14f970
Jhonatan Martins <jhonatanbmartins@gmail.com>
2017-11-13 11:46:05 -0200

    Retrieve sms tests

--- spec/lib/totalvoice_ruby/sms_spec.rb
+++ spec/lib/totalvoice_ruby/sms_spec.rb
@@ -10,12 +10,29 @@ RSpec.describe TotalvoiceRuby::Sms do
       .to_return(status: 200, body: sms_json)
     end
 
-    it "Api response == 200" do
+    it "Returns success" do
       sms = TotalvoiceRuby::Sms.new("1234")
       response = sms.create(@destination_number, @message)
       expect(response["status"]).to eq(200)
       expect(response["sucesso"]).to eq(true)
       expect(response["mensagem"]).to eq(@message)
+    end
+  end
+
+  describe "GET retrieve" do
+    let(:sms_json) { File.read("spec/fixtures/sms/retrieved.json") }
+    before do
+      @id = "123456"
+      stub_request(:get, "https://api.totalvoice.com.br/sms/#{@id}?access_token=1234")
+      .to_return(status: 200, body: sms_json)
+    end
+
+    it "Returns sms with success" do
+      sms = TotalvoiceRuby::Sms.new("1234")
+      response = sms.retrieve(@id)
+      expect(response["status"]).to eq(200)
+      expect(response["sucesso"]).to eq(true)
+      expect(response["mensagem"]).to eq("dados retornados com sucesso")
     end
   end
 end
