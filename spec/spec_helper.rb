203565b034da10fd2372d86958142b2288b472aa
Jhonatan Martins <jhonatanbmartins@gmail.com>
2017-11-13 09:03:08 -0200

    Add webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require "bundler/setup"
 require "totalvoice_ruby"
+require "webmock/rspec"
+
+WebMock.disable_net_connect!(allow_localhost: true)
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
